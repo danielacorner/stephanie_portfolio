970fc80ea81b9c61f86dc7557dae2bc8feae6af6
Daniel Corner <dcorn068@gmail.com>
2018-11-16 00:45:09 -0500

    Updates

---
+++ precache-manifest.35b6a0b006994d894e3ef22c34a092af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "54cd0c025f5bf0ccc66d",
+    "url": "/stephanie_portfolio/static/js/runtime~main.54cd0c02.js"
+  },
+  {
+    "revision": "fd5b3dffe4a314cbf629",
+    "url": "/stephanie_portfolio/static/js/main.fd5b3dff.chunk.js"
+  },
+  {
+    "revision": "659615c23c9827b95dd0",
+    "url": "/stephanie_portfolio/static/js/1.659615c2.chunk.js"
+  },
+  {
+    "revision": "fd5b3dffe4a314cbf629",
+    "url": "/stephanie_portfolio/static/css/main.c3f84589.chunk.css"
+  },
+  {
+    "revision": "4aff4022d7cafdefcfef259c3965e5b2",
+    "url": "/stephanie_portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.77c1e8156b4a795b11995c210e8a0f4b.js"
+  "/stephanie_portfolio/precache-manifest.35b6a0b006994d894e3ef22c34a092af.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/stephanie_portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
